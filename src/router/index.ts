b0d9117957a91b34c9cca9ea238acafb6842f1a5
liangxiangtao <liangliang.too@foxmail.com>
2025-05-30 18:02:54 +0800

    count demo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Count from "../views/count.vue";
 
 const routes = [
-  { path: '/', component: Home, meta: { requiresAuth: true }},
-  { path: '/login', component: Login }
-]
+  { path: "/", component: Home, meta: { requiresAuth: true } },
+  { path: "/login", component: Login },
+  { path: "/count", component: Count },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
 router.beforeEach((to, _, next) => {
-  const isAuth = !!localStorage.getItem('token')
+  const isAuth = !!localStorage.getItem("token");
   if (to.meta.requiresAuth && !isAuth) {
-    next('/login')
+    next("/login");
   } else {
-    next()
+    next();
   }
-})
+});
 
-export { router }
+export { router };
---
+++ src/views/count.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="mt-10">
+    <h1 class="text-2xl font-bold mb-4">计数器</h1>
+    <div class="flex items-center space-x-4 mb-6">
+      <v-btn color="primary" @click="decrement">-</v-btn>
+      <span class="text-xl font-mono">{{ count }}</span>
+      <v-btn color="primary" @click="increment">+</v-btn>
+    </div>
+    <v-btn color="secondary" @click="reset">重置</v-btn>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+
+const reset = () => {
+  count.value = 0;
+};
+</script>
